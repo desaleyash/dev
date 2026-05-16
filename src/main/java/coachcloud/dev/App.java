bd588be053d1c60c7bfe3a4f71caeca5623e7d63
Yash Desale <yourgithubemail@gmail.com>
2026-05-16 13:54:27 +0530

    Commit main with test folder with POM

---
+++ src/main/java/coachcloud/dev/App.java
@@ -0,0 +1,13 @@
+package coachcloud.dev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
